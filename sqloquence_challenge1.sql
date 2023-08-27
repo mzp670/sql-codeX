fe72cd3230ede84110e6dc58972baa213abcad0f
mzp670 <141281179+mzp670@users.noreply.github.com>
2023-08-28 09:50:13 +1200

    Update sqloquence_challenge1.sql

--- sqloquence_challenge1.sql
+++ sqloquence_challenge1.sql
@@ -1,6 +1,6 @@
-#  From a database containing at least one table named 'service_request,' create an SQL query to provide a list of agencies, 
-#  along with the count of different complaint types they handle based on our data. The result should be ordered in descending order, 
-#  starting with the agency that handles the most complaint types and ending with the agency that handles the least.
+--  From a database containing at least one table named 'service_request,' create an SQL query to provide a list of agencies, 
+--  along with the count of different complaint types they handle based on our data. The result should be ordered in descending order, 
+--  starting with the agency that handles the most complaint types and ending with the agency that handles the least.
 
 select 
   distinct agency_name, 
